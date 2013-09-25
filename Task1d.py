4eac5bb4d5969700489a0bcae5a5a9cb0ebf1528
merrick.heley <merrick.heley@gmail.com>
2013-09-26 01:07:43 +1000

    reworking 1d

--- Task1d.py
+++ Task1d.py
@@ -1,62 +1,12 @@
 import math
 import numpy
 import pylab
+import Newtons  
 
 def floatcmp(a, b):
     if (abs(a-b) < 1e-6):
         return True
     return False
-
-def afRange(start, stop, step):
-    
-    val = start
-    retList = []
-    
-    while (val < stop and floatcmp(val, stop) == False):
-        retList.append(val)
-        val += step
-    
-    return retList
-    
-def genXMatrix(f, y, BVs, h):
-    
-    size = len(y)
-    xV = afRange(BVs[0][0]+h, BVs[-1][0], h)
-    
-    XList =  [f(BVs[0][1], y[0], y[1], xV[0])]
-    XList += [f(y[i-1], y[i], y[i+1], xV[i]) for i in range(1, size-1)]
-    XList += [f(y[-2], y[-1], BVs[-1][1], xV[-1])]
-    
-    return numpy.array(XList)
-    
-def genJMatrix(f, devs, x, BVs, h):
-    
-    size = len(x)
-    border = (devs-1)/2
-    xV =afRange(BVs[0][0]+h, BVs[-1][0], h)
-    
-    YList = [[0]*i +  f(x[i], xV[i]) + [0]*(size - 1 - i) for i in range(0, size)]
-
-    return numpy.matrix(YList)[:, border:-border] 
-
-def solveBVP(fM, fV, h, BVs, dSize, guess):
-    steps = int((BVs[-1][0] - BVs[0][0])/h)-1
-    
-    P = numpy.array([guess]*steps)
-    
-    for n in range(1000):
-        a = genJMatrix(fM, dSize, P, BVs, h)
-        b = genXMatrix(fV, P, BVs, h)
-        
-        Q = P + numpy.linalg.solve(a, b)
-        
-        norm = numpy.linalg.norm(Q-P)
-        if norm < 1e-6:
-            return P, norm, n
-        else:
-            P = Q
-
-    return P, norm, n #None, n
 
 if __name__ == '__main__':
     #[ 1.1,         1.2,         1.3,         1.4,         1.5,        1.6,         1.7]
@@ -65,26 +15,18 @@ if __name__ == '__main__':
     h = 0.1
     BVs = [(1, 1), (1.8, 3)]
     
-    fMat = lambda y, x, h=h: [1, - 2 + (h**2)*(((math.cos(y)+2)**2)/(y+1e-10) - 9*(x**3)*(y**2)), 1]
-    fVal = lambda y1, y2, y3, x, h=h: -(y1 - 2*y2 + (h**2)*((math.cos(y2) + 2)**2 - 9*(x**3)*(y2**2)) + y3);
+    f = lambda y1, y2, y3, x, h=h: y1 - 2*y2 + (h**2)*((math.cos(y2) + 2)**2 - 9*(x**3)*(y2**2)) + y3
     
-#     print fVal(0.85787217, 0.78041193, 0.72795581, 1.2, 0.1)
-#     print fVal(0.78041193, 0.72795581, 0.70596757, 1.3) 
-#     print fVal(0.72795581, 0.70596757, 0.73336866, 1.4)
-#     print fVal(0.73336866, 0.85952722,  1.26134775, 1.6)
-#     print "---------"
-#     print fVal(0.36350875,  -0.06847174,-0.07660059, 1.3)
-#     print fVal(-0.03255478, 0.06302292,  0.78967751, 1.6)   
+    funcList = []
     
-    xList = afRange(BVs[0][0], BVs[-1][0]+h, h)   
-    yList, norm, n = solveBVP(fMat, fVal, h, BVs, 3, 2.0)
-     
-    print yList, norm, n
-    yList = yList.tolist()
+    funcList.append(lambda y1, y2, y3, y4, y5, y6, y7: f(1 , y1, y2,  1.1, 0.1))
+    funcList.append(lambda y1, y2, y3, y4, y5, y6, y7: f(y1, y2, y3,  1.2, 0.1))
+    funcList.append(lambda y1, y2, y3, y4, y5, y6, y7: f(y2, y3, y4,  1.3, 0.1))
+    funcList.append(lambda y1, y2, y3, y4, y5, y6, y7: f(y3, y4, y5,  1.4, 0.1))
+    funcList.append(lambda y1, y2, y3, y4, y5, y6, y7: f(y4, y5, y6,  1.5, 0.1))
+    funcList.append(lambda y1, y2, y3, y4, y5, y6, y7: f(y5, y6, y7,  1.6, 0.1))
+    funcList.append(lambda y1, y2, y3, y4, y5, y6, y7: f(y6, y7,  3,  1.7, 0.1))
     
-    yList = [BVs[0][1]] + yList + [BVs[-1][1]] 
-
-    pylab.plot(xList, yList, 'b-')
-    pylab.grid()
-    pylab.show()
+    P = numpy.array([10.0]*7)
+    print Newtons.iterativeSolve(funcList, P, h)
     
--- Task2a.py
+++ Task2a.py
@@ -3,6 +3,7 @@ import numpy
 import Newtons
 
 if __name__ == '__main__':
+    # http://www.wolframalpha.com/input/?i=solve+-15x%5E2-57xy-67y%5E2+%3D+-26864190700%2C+x%5E3+%2B+3*y*x%5E2+-+2*x*y%5E2+-+7y%5E3+%3D+-604894015496000
     
     f = [lambda x,y: -(x**3 + 3*(x**2)*y - 2*x*(y**2) - 7*(y**3) + 604894015496000), 
          lambda x,y: -(-15*(x**2) - 57*x*y - 67*(y**2) + 26864190700)]

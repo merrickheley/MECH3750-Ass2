a0fd3b9d32883d7e11022efd83945e1fc80a34f8
merrick.heley <merrick.heley@gmail.com>
2013-09-28 16:34:56 +1000

    1c is solved

--- Task1c.py
+++ Task1c.py
@@ -1,5 +1,8 @@
 import numpy
 import pylab
+import sys
+
+#numpy.set_printoptions(threshold=numpy.nan)
 
 def floatcmp(a, b):
     if (abs(a-b) < 1e-6):
@@ -16,61 +19,80 @@ def afRange(start, stop, step):
         val += step
     
     return retList
+
+def getIndex(val, BVs, h):
+    return int(abs(val - BVs[0][0])/h)
     
 def genXMatrix(f, BVs, h):
     
     XList = []
     
-    XList += [f(x, h) for x in afRange(BVs[0][0]+h, BVs[-1][0], h)]
+    XList += [f(x, h) for x in afRange(BVs[0][0], BVs[-1][0]+h, h)]
+    
+    for BVi in BVs:
+        XList[getIndex(BVi[0], BVs, h)] = BVi[1]
     
     return numpy.array(XList)
     
 def genJMatrix(f, BVs, h):
 
-    xV =afRange(BVs[0][0]+h, BVs[-1][0], h)
+    xV =afRange(BVs[0][0], BVs[-1][0]+h, h)
     size = len(xV)
     
-    
     YList = [[0]*i +  f(x, h) + [0]*(size - 1 - i) for i,x in enumerate(xV)]
+    YMat = numpy.matrix(YList)[:, 2:-2]
     
-    return numpy.matrix(YList)[:, 2:-2]
+    for BVi in BVs:
+        i = getIndex(BVi[0], BVs, h)
+        YMat[i, :] = numpy.zeros((1, size))
+        YMat[i, i] = 1     
+    
+    return YMat
 
 def f(x, h):
-    return [1,
-            -2 + 2*h + 4*x*(h**2),
-            -4*h + 2*(h**3)*(8*x + 3),
-            2 + 2*h - 4*x*(h**2),
-            -1]
+    return [1.0/(2*(h**3)),
+            -2.0/(2*h**3) + 1.0/(h**2) + 4.0*x/(2*h),
+            -2.0/(h**2) + (8.0*x + 3.0),
+            2.0/(2*h**3) + 1.0/(h**2) - 4.0*x/(2*h),
+            -1.0/(2*(h**3))]
+    
+def fForward(x, h):
+    return [0.0,
+            1.0/(h**3) + 1.0/(h**2) + 4.0*x/(2*h),
+            -3.0/(h**3) - 2.0/(h**2) + (8.0*x + 3.0),
+            3.0/(h**3) + 1.0/(h**2) - 4.0*x/(2*h),
+            -1.0/(h**3)]
 
+def fBackward(x, h):
+    return [1.0/(h**3),
+            -3.0/(h**3) + 1.0/(h**2) + 4.0*x/(2*h),
+            3.0/(h**3) - 2.0/(h**2) + (8.0*x + 3.0),
+            -1.0/(h**3) + 1.0/(h**2) - 4.0*x/(2*h),
+            0.0]
+
+    
 if __name__ == '__main__':
     
-    h = 0.001
-    BV = [(-1, -10), (0.5, 1), (1.5, -3)]
+    h = 0.1
+    BVs = [(-1, -10), (0.5, 1), (1.5, -3)]
     
     Xvals = []
-    Yvals = []
+          
+    Xi = lambda x,h: (x**2)
     
-    for i in range(2):
-        BVs= BV[i:i+2]
-        print BVs
+    J = genJMatrix(f, BVs, h)
+    X = genXMatrix(Xi, BVs, h)
+    
+    # Handle difference approximation at boundaries
+    J[1,  0:5] = fForward(BVs[0][0]+h, h)
+    J[-2, -5:] = fBackward(BVs[-1][0]+h, h)
         
-        Xi = lambda x,h: 2*(h**3)*(x**2)
-        
-        X = genXMatrix(Xi, BVs, h)
-        J = genJMatrix(f,  BVs, h)
-        
-        X[0] -= BVs[0][1] + (-2 + 2*h + 4*(BVs[0][0]+h)*(h**2))*BVs[0][1]
-        X[1] -= BVs[0][1]
-        
-        X[-2] -= -BVs[-1][1]
-        X[-1] -= (2 + 2*h - 4*(BVs[-1][0]-h)*(h**2))*BVs[-1][1] - BVs[-1][1]
-        
-        print i
-        Xvals += afRange(BVs[0][0]+h, BVs[-1][0], h)
-        Yvals += list(numpy.linalg.solve(J, X))
-     
-    pylab.title("Solution to BVP")
-    pylab.xlabel("x"); pylab.ylabel("y")
-    Plot1, = pylab.plot(Xvals, Yvals, 'b-')
+    #numpy.savetxt(sys.stdout, J, '%5.2f')
+    
+    xlist = afRange(BVs[0][0], BVs[-1][0]+h, h) 
+    ylist = numpy.linalg.solve(J, X)
+    
+    pylab.plot(xlist, ylist, 'b-')
     pylab.grid()
     pylab.show()
+    
